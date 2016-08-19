a9915849d782ea15f23168792376240f48fe28ed
Shashikant Mokkarala <msashikanth@pkrmsezdc.com>
2016-08-19 21:43:01 +0530

    Showing the properties to user for confirmation.

--- generator-jhipster-usm-server/generators/index.js
+++ generator-jhipster-usm-server/generators/index.js
@@ -142,85 +142,119 @@ module.exports = yeoman.Base.extend({
     this.projectAuthor = 'ITOS';
 
   },
+  prompting: function() {
+    var done = this.async();
+    this.log("\nPlease verify the below details: ");
+
+    this.log("\nProjectName: " + this.projectName);
+    this.log("\nProjectTLA: " + this.tla);
+    this.log("\nPackage: " + this.packageName);
+    this.log("\nRestURL: " + this.methodURL);
+    this.log("\nExternalRestURL: " + this.url);
+    this.log("\nEntityClassName: " + this.entityClass);
+    this.log("\nEntity Beans:");
+    for (var i = 0; i < this.applicationPOJOs.length; i++) {
+      this.log(this.applicationPOJOs[i]);
+      this.log("\n");
+    }
+
+    this.response = [];
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'confirmCreation',
+      message: 'Are all the above details verified?',
+      default: true
+    }];
+
+    this.prompt(prompts, function(props) {
+      this.confirmCreation = props.confirmCreation;
+      done();
+    }.bind(this));
+
+  },
   writing: {
-
     app: function() {
+      if (this.confirmCreation) {
 
-      var underscoreParams = {
-        evaluate: /\<\%([\s\S]+?)\%\>/g,
-        interpolate: /\<\%\=([\s\S]+?)\%\>/g,
-        escape: /\<-([\s\S]+?)\>/g
-      };
-      var baseProjectPath = 'base-project/';
+        var underscoreParams = {
+          evaluate: /\<\%([\s\S]+?)\%\>/g,
+          interpolate: /\<\%\=([\s\S]+?)\%\>/g,
+          escape: /\<-([\s\S]+?)\>/g
+        };
+        var baseProjectPath = 'base-project/';
 
-      var genericTemplateFiles = ['_pom.xml'];
-      var packagePath = this.packageName.replace(/\./g, '/');
-      var javaSrcPath = baseProjectPath + 'src/main/java/'
-      var javaSrcTestPath = baseProjectPath + 'src/test/java/'
-      var javaPath = 'src/main/java/' + packagePath + '/';
-      var javaTestPath = 'src/test/java/' + packagePath + '/';
-      this.host = this.configYMLObject && this.configYMLObject.host ? this.configYMLObject.host : {
-        dev: '',
-        local: ''
-      };
+        var genericTemplateFiles = ['_pom.xml'];
+        var packagePath = this.packageName.replace(/\./g, '/');
+        var javaSrcPath = baseProjectPath + 'src/main/java/'
+        var javaSrcTestPath = baseProjectPath + 'src/test/java/'
+        var javaPath = 'src/main/java/' + packagePath + '/';
+        var javaTestPath = 'src/test/java/' + packagePath + '/';
+        this.host = this.configYMLObject && this.configYMLObject.host ? this.configYMLObject.host : {
+          dev: '',
+          local: ''
+        };
 
-      //console.log(this.configYMLObject);
+        //console.log(this.configYMLObject);
 
-      // if(this.configYMLObject){
-      //   host.dev = this.configYMLObject.host && this.configYMLObject.host.dev ? this.configYMLObject.host.dev: '';
-      //   host.local = this.configYMLObject.local && this.configYMLObject.host.local ? this.configYMLObject.host.local: '';
-      // }
+        // if(this.configYMLObject){
+        //   host.dev = this.configYMLObject.host && this.configYMLObject.host.dev ? this.configYMLObject.host.dev: '';
+        //   host.local = this.configYMLObject.local && this.configYMLObject.host.local ? this.configYMLObject.host.local: '';
+        // }
 
-      for (var f in genericTemplateFiles)
-        this.template(baseProjectPath + genericTemplateFiles[f], genericTemplateFiles[f].substr(1, 500), this);
+        for (var f in genericTemplateFiles)
+          this.template(baseProjectPath + genericTemplateFiles[f], genericTemplateFiles[f].substr(1, 500), this);
 
-      // Directories
-      this.directory(baseProjectPath + 'src/main/webapp/', 'src/main/webapp/');
-      this.directory(baseProjectPath + 'src/test/resources/', 'src/test/resources/');
-      this.directory(baseProjectPath + 'src/main/java/package/', 'src/main/java/' + packagePath + '/');
-      this.directory(baseProjectPath + 'src/test/java/package/', 'src/test/java/' + packagePath + '/');
-
-
-      // src/main/webapp files
-      this.template(baseProjectPath + '_web.xml', 'src/main/webapp/WEB-INF/web.xml', this);
-      this.template(baseProjectPath + 'src/main/webapp/config/config.properties', 'src/main/webapp/config/config.properties', this);
+        // Directories
+        this.directory(baseProjectPath + 'src/main/webapp/', 'src/main/webapp/');
+        this.directory(baseProjectPath + 'src/test/resources/', 'src/test/resources/');
+        this.directory(baseProjectPath + 'src/main/java/package/', 'src/main/java/' + packagePath + '/');
+        this.directory(baseProjectPath + 'src/test/java/package/', 'src/test/java/' + packagePath + '/');
 
 
-      // src/main/resources files
-      this.template(baseProjectPath + 'src/main/resources/logback.xml', 'src/main/resources/logback.xml', this);
-      this.template(baseProjectPath + 'src/main/resources/log4j2.xml', 'src/main/resources/log4j2.xml', this);
-      this.template(baseProjectPath + 'src/main/resources/_dev.properties', 'src/main/resources/dev.properties', this);
-      this.template(baseProjectPath + 'src/main/resources/_local.properties', 'src/main/resources/local.properties', this);
-      this.template(baseProjectPath + 'src/main/resources/spring/application-config.xml', 'src/main/resources/spring/application-config.xml', this);
-      this.template(baseProjectPath + 'src/main/resources/spring/mvc-config.xml', 'src/main/resources/spring/mvc-config.xml', this);
-      this.template(baseProjectPath + 'src/main/resources/spring/usm-application-context.xml', 'src/main/resources/spring/usm-application-context.xml', this);
+        // src/main/webapp files
+        this.template(baseProjectPath + '_web.xml', 'src/main/webapp/WEB-INF/web.xml', this);
+        this.template(baseProjectPath + 'src/main/webapp/config/config.properties', 'src/main/webapp/config/config.properties', this);
 
 
-      // src/main/java files
-      this.template(javaSrcPath + 'package/util/USMDashboardMapper.java', javaPath + 'util/USMDashboardMapper.java', this);
-      this.template(javaSrcPath + 'package/controller/ApplicationController.java', javaPath + 'controller/ApplicationController.java', this);
-      this.template(javaSrcPath + 'package/controller/BaseController.java', javaPath + 'controller/BaseController.java', this);
+        // src/main/resources files
+        this.template(baseProjectPath + 'src/main/resources/logback.xml', 'src/main/resources/logback.xml', this);
+        this.template(baseProjectPath + 'src/main/resources/log4j2.xml', 'src/main/resources/log4j2.xml', this);
+        this.template(baseProjectPath + 'src/main/resources/_dev.properties', 'src/main/resources/dev.properties', this);
+        this.template(baseProjectPath + 'src/main/resources/_local.properties', 'src/main/resources/local.properties', this);
+        this.template(baseProjectPath + 'src/main/resources/spring/application-config.xml', 'src/main/resources/spring/application-config.xml', this);
+        this.template(baseProjectPath + 'src/main/resources/spring/mvc-config.xml', 'src/main/resources/spring/mvc-config.xml', this);
+        this.template(baseProjectPath + 'src/main/resources/spring/usm-application-context.xml', 'src/main/resources/spring/usm-application-context.xml', this);
 
-      for (var index = 0; index < this.applicationPOJOs.length; index++) {
-        var applicationPOJO = this.applicationPOJOs[index];
-        console.log(applicationPOJO);
 
-        this.template(javaSrcPath + 'Entity.java', javaPath + 'dto/' + applicationPOJO.className + '.java', {
-          packageName: this.packageName,
-          className: applicationPOJO.className,
-          fields: applicationPOJO.variables
-        });
+        // src/main/java files
+        this.template(javaSrcPath + 'package/util/USMDashboardMapper.java', javaPath + 'util/USMDashboardMapper.java', this);
+        this.template(javaSrcPath + 'package/controller/ApplicationController.java', javaPath + 'controller/ApplicationController.java', this);
+        this.template(javaSrcPath + 'package/controller/BaseController.java', javaPath + 'controller/BaseController.java', this);
+
+        for (var index = 0; index < this.applicationPOJOs.length; index++) {
+          var applicationPOJO = this.applicationPOJOs[index];
+          console.log(applicationPOJO);
+
+          this.template(javaSrcPath + 'Entity.java', javaPath + 'dto/' + applicationPOJO.className + '.java', {
+            packageName: this.packageName,
+            className: applicationPOJO.className,
+            fields: applicationPOJO.variables
+          });
+        }
+        this.template(javaSrcPath + 'package/service/ApplicationService.java', javaPath + 'service/ApplicationService.java', this);
+
+
+        // src/test/java files
+        this.template(javaSrcTestPath + 'package/controller/ApplicationControllerTest.java', javaTestPath + 'controller/ApplicationControllerTest.java', this);
+        this.template(javaSrcTestPath + 'package/service/ApplicationServiceTest.java', javaTestPath + 'service/ApplicationServiceTest.java', this);
+
+        // src/test/resources files
+        this.template(baseProjectPath + 'src/test/resources/config/config.test.properties', 'src/test/resources/config/config.test.properties', this);
+
+      }else{
+        process.exit(1);
       }
-      this.template(javaSrcPath + 'package/service/ApplicationService.java', javaPath + 'service/ApplicationService.java', this);
-
-
-      // src/test/java files
-      this.template(javaSrcTestPath + 'package/controller/ApplicationControllerTest.java', javaTestPath + 'controller/ApplicationControllerTest.java', this);
-      this.template(javaSrcTestPath + 'package/service/ApplicationServiceTest.java', javaTestPath + 'service/ApplicationServiceTest.java', this);
-
-      // src/test/resources files
-      this.template(baseProjectPath + 'src/test/resources/config/config.test.properties', 'src/test/resources/config/config.test.properties', this);
-
     },
 
     projectfiles: function() {}
