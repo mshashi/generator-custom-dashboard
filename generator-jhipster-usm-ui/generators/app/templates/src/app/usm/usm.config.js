5d52f541554b69aaccf116bc660a4bac3ee362e3
Shashikant Mokkarala <mokkarala.shashi@gmail.com>
2016-10-20 15:28:16 +0530

    Added grid to ui

--- generator-jhipster-usm-ui/generators/app/templates/src/app/usm/usm.config.js
+++ generator-jhipster-usm-ui/generators/app/templates/src/app/usm/usm.config.js
@@ -3,6 +3,9 @@ function welcomeConfig($routeProvider) {
     $routeProvider.when('/welcome', {
         templateUrl: 'usm/usm.html',
         controller: 'USMCtrl'
+    }).when("/edit", {
+      templateUrl: 'usm/usmGrid.html',
+      controller: 'USMGridCtrl'
     });
 }
 
